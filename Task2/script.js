cc8629e426bdcea65230e4c64b463170be1d1c91
NojusVys <nojusv@gmail.com>
2022-11-18 16:02:26 +0200

    #2 does not work :(

--- Task2/script.js
+++ Task2/script.js
@@ -3,13 +3,15 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
-let clicks = 0;
-const mygtukas = document
-    .querySelector(".btn")
-    .addEventListener("click", hello);
 
-function hello() {
-    const states = mygtukas.getElementById("btn_state");
-    clicks += 1;
-    console.log(states);
-}
+window.onload = function () {
+    let clicks = 0;
+    const mygtukas = document.querySelector(".btn");
+    mygtukas.addEventListener("click", skaiciavimas);
+
+    function skaiciavimas() {
+        clicks++;
+        document.getElementById("btn_state").value = clicks;
+        return clicks;
+    }
+};
